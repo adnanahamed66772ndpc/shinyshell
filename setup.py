855d177f9f7a9bd47fd09f3537f40e2722e2bf79
Adnan Ahamed Himal <shovroahamed6677@zohomail.com>
2026-07-29 14:15:52 +0100

    v0.6.0: modular + tests + CI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="shinyshell",
-    version="0.5.1",
+    version="0.6.0",
     author="Adnan Ahamed Himal",
     author_email="hello@adnanahamedhimal.com",
     description="Python library for beautiful terminal output — colored printing, tables, progress bars, syntax highlighting, bar charts, QR codes, and more. Zero dependencies.",
